da628d3b8660c8158c411ad87b9cac6ba0dbeca8
artembark <artembark@gmail.com>
2021-11-19 14:04:29 +0300

    drum add

--- lib/main.dart
+++ lib/main.dart
@@ -5,43 +5,70 @@ void main() {
   runApp(const XyloApp());
 }
 
-class XyloApp extends StatelessWidget {
+class XyloApp extends StatefulWidget {
   const XyloApp({Key? key}) : super(key: key);
+
+  @override
+  State<XyloApp> createState() => _XyloAppState();
+}
+
+class _XyloAppState extends State<XyloApp> {
+  bool instrument = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            buildExpanded(
-                color: Colors.red, fileName: 'magic_music.mp3', title: ''),
-            buildExpanded(
-                color: Colors.green, fileName: 'magic_music.mp3', title: ''),
-            buildExpanded(
-                color: Colors.blue, fileName: 'magic_music.mp3', title: ''),
-          ],
-        )),
+        appBar: AppBar(
+          title: GestureDetector(
+            onTap: () {
+              setState(() {
+                instrument = !instrument;
+              });
+            },
+            child: Text('Change instrument'),
+          ),
+        ),
+        body: drumMachine(),
       ),
     );
   }
 
-  Expanded buildExpanded({
+  GridView drumMachine() {
+    return GridView(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      children: [
+        buildExpanded(
+            color: Colors.red, fileName: 'magic_music.mp3', title: ''),
+        buildExpanded(
+            color: Colors.green, fileName: 'magic_music.mp3', title: ''),
+        buildExpanded(
+            color: Colors.blue, fileName: 'magic_music.mp3', title: ''),
+        buildExpanded(
+            color: Colors.black, fileName: 'magic_music.mp3', title: ''),
+        buildExpanded(
+            color: Colors.teal, fileName: 'magic_music.mp3', title: ''),
+        buildExpanded(
+            color: Colors.yellow, fileName: 'magic_music.mp3', title: ''),
+        buildExpanded(
+            color: Colors.orange, fileName: 'magic_music.mp3', title: ''),
+      ],
+    );
+  }
+
+  buildExpanded({
     required Color color,
     required String fileName,
     required String title,
   }) {
-    return Expanded(
-      child: TextButton(
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(color)),
-        onPressed: () {
-          playFile(fileName);
-        },
-        child: Text(title),
-      ),
+    return TextButton(
+      style:
+          ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(color)),
+      onPressed: () {
+        playFile(fileName);
+      },
+      child: Text(title),
     );
   }
 
